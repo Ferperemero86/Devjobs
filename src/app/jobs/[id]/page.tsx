f8ef0ca92e537e487315cab687c2cd793eac7bb2
Ferperemero86 <fernandoperezmerono@gmail.com>
2023-07-18 10:04:07 +0100

    Add job Details page design

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import Image from "next/image"; 
+import Link from "next/link";
 
 import { ThemeContext } from "@/contexts/ThemeContext";
-import { useAppSelector, useAppDispatch } from "@/state/hooks";
 
 import data from "../../../../data";
 
@@ -30,12 +30,12 @@ type Job = {
   };
 };
 
-interface jobDetails {
-	jobDetails: Job
-}
-
 type Params = {
 	id: string
+}
+
+interface jobDetails {
+	jobDetails: Job
 }
 
 interface PageProps {
@@ -58,16 +58,94 @@ const JobDetailsHeader = ({jobDetails}: jobDetails) => {
 	}
 
   return (
-		<div className={`${bgColor} relative max-w-sm p-5 rounded-md flex justify-center text-center mx-auto`}>
-    	<span style={bgStyles} className={`absolute -top-7 flex justify-center items-center w-16 h-16 rounded-md`}>
-				<span className={`w-10 h-10 relative`}>
+		<div className={`${bgColor} relative -top-5 w-11/12 p-5 rounded-md flex justify-center text-center mx-auto md:-top-10 md:max-w-6xl md:h-40`}>
+    	<span style={bgStyles} className={`absolute -top-7 flex justify-center items-center w-16 h-16 rounded-md md:left-0 md:top-0 md:w-40 md:h-full md:rounded-none`}>
+				<span className={`w-10 h-10 relative lg:w-16 lg:h-16`}>
 					{logo ? <Image src={logo} alt="job logo" fill={true} priority={true} /> : null}
 				</span>
     	</span>
 			<div className="mt-10">
-		  	<p className={`${titleColor} text-fifthColor text-lg font-bold mt-1`}>{company}</p>
-      	<p className="text-fourthColor mt-1">{company}.com</p>
-      	<a className="text-thirdColor bg-thirdColor bg-opacity-25 font-bold mt-3 rounded-md p-2 inline-block" href={website} target="_blank">Company Site</a>
+		  	<p className={`${titleColor} text-fifthColor text-lg font-bold mt-1 md:absolute md:left-48 md:top-6`}>{company}</p>
+      	<p className="text-fourthColor mt-1 md:absolute md:left-48 md:top-14">{company}.com</p>
+				<Link className={`${titleColor} mt-14 md:absolute md:left-48 md:top-14 underline`} href="/">Back to Search</Link>
+      	<a className="text-thirdColor bg-thirdColor bg-opacity-25 font-bold mt-3 rounded-md p-2 inline-block md:absolute md:right-10" href={website} target="_blank">Company Site</a>
+			</div>
+  	</div>
+	)
+};
+
+const JobDetailsBody = ({jobDetails}: jobDetails) => {
+	const {theme} = useContext(ThemeContext);
+  const {mainBg} = theme;
+	const bgColor = mainBg ==="bg-secondColor" ? "bg-fifthColor" : "bg-white";
+	const titleColor = mainBg ==="bg-secondColor" ?  "text-white" : "text-fifthColor";
+
+	const time = jobDetails?.postedAt;
+	const contract = jobDetails?.contract;
+	const title = jobDetails?.position;
+	const location = jobDetails?.location;
+	const websiteUrl = jobDetails?.website;
+	const description = jobDetails?.description;
+	const requirements = jobDetails?.requirements;
+	const role = jobDetails?.role;
+
+
+  return (
+		<div className={`${bgColor} w-11/12 relative p-5 rounded-md mx-auto md:py-14 md:max-w-6xl`}>
+    	<div className="w-full">
+				<div className="w-full text-left">
+					<p className="text-fourthColor">{time} . {contract}</p>
+					<p className={`${titleColor} text-fifthColor text-lg font-bold mt-2 md:font`}>{title}</p>
+					<p className="text-thirdColor font-bold mt-2">{location}</p>
+				</div>
+				<a className="mt-10 inline-block bg-thirdColor text-white text-center rounded-sm py-3 w-11/12 md:w-40 md:m-0 md:absolute md:right-10 md:top-20" href={websiteUrl} target="_blank">Apply Now</a>
+			</div>
+			<div className="w-full mt-10 md:mt-10">
+				<p className="text-fourthColor">{description}</p>
+				<div className="mt-5">
+					<h2 className={`${titleColor} text-fifthColor text-lg font-bold mt-2 mb-5 md:font`}>Requirements</h2>
+					<p className="text-fourthColor">{requirements.content}</p>
+					<ul className="list-disc w-11/12 relative left-10 mt-5">
+						{requirements.items.map((item, idx) => (
+							<li className="text-fourthColor mt-2" key={idx}>{item}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			<div className="w-full mt-10 md:mt-10">
+				<p className="text-fourthColor">{description}</p>
+				<div className="mt-5">
+					<h2 className={`${titleColor} text-fifthColor text-lg font-bold mt-2 mb-5 md:font`}>What You Will Do</h2>
+					<p className="text-fourthColor">{role.content}</p>
+					<ul className="list-decimal w-11/12 relative left-10 mt-5 marker:text-thirdColor marker:font-bold">
+						{role.items.map((item, idx) => (
+							<li className="text-fourthColor mt-2 pl-2" key={idx}>{item}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+  	</div>
+	)
+}
+
+const JobDetailsFooter = ({jobDetails}: jobDetails) => {
+	const {theme} = useContext(ThemeContext);
+  const {mainBg} = theme;
+	const bgColor = mainBg ==="bg-secondColor" ? "bg-fifthColor" : "bg-white";
+	const titleColor = mainBg ==="bg-secondColor" ?  "text-white" : "text-fifthColor";
+
+	const title = jobDetails?.position;
+	const company = jobDetails?.company;
+	const websiteUrl = jobDetails?.website;
+
+  return (
+		<div className={`${bgColor} w-full mt-20 relative p-5 mx-auto md:py-5`}>
+    	<div className="w-full py-5">
+				<div className="w-full text-left">
+					<p className={`${titleColor} hidden text-fifthColor text-lg font-bold mt-2 md:font md:block`}>{title}</p>
+					<p className="hidden text-thirdColor font-bold mt-2 md:block">{company}</p>
+				</div>
+				<a className="inline-block bg-thirdColor text-white text-center rounded-sm py-3 w-11/12 md:w-40 md:m-0 md:absolute md:right-10 md:-translate-y-1/2 md:top-1/2" href={websiteUrl} target="_blank">Apply Now</a>
 			</div>
   	</div>
 	)
@@ -76,16 +154,16 @@ const JobDetailsHeader = ({jobDetails}: jobDetails) => {
 export default function Page({params}: PageProps) {
 	const {theme} = useContext(ThemeContext);
   const {mainBg} = theme;
-	const bgColor = mainBg ==="bg-secondColor" ? "bg-fifthColor" : "bg-white";
-	const titleColor = mainBg ==="bg-secondColor" ?  "text-white" : "text-fifthColor";
-
+	
 	const jobParamId = parseInt(params.id);
 	const jobDetails = data.find(job => job.id === jobParamId);
 
 	
 	return (
-		<main className={`${mainBg} min-h-screen py-20`}>
+		<main className={`${mainBg} min-h-screen`}>
 			 {jobDetails ? <JobDetailsHeader jobDetails={jobDetails} /> : null}
+			 {jobDetails ? <JobDetailsBody   jobDetails={jobDetails} /> : null}
+			 {jobDetails ? <JobDetailsFooter   jobDetails={jobDetails} /> : null}
 		</main>
 	)
 }
